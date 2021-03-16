b0e3ef9aa188a29d2c6d5eaccf13035a4624b6c5
genisgilavert <anonymousheimer@gmail.com>
2021-03-16 17:07:28 +0100

    Init Repository, Tennis project

--- Tenis/controller/GameManager.cs
+++ Tenis/controller/GameManager.cs
@@ -9,6 +9,8 @@ using Tenis.data.abstractClasses;
 namespace Tenis.controller{
     class GameManager{
         
+        // testing Git to delete
+
         private DataManager dataManager;
         private const int numberPlayersTenis = 2;
         private Boolean isReadyInitGame;
